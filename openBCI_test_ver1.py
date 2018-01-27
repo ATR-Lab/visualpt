604fcb396ea1589a63eb6032fc6294c9cd532983
Do Yeon Kim <dkim@Dos-MacBook-Pro.local>
2018-01-27 05:39:47 -0500

    Add pandas df

--- openBCI_test_ver1.py
+++ openBCI_test_ver1.py
@@ -1,9 +1,16 @@
+# Authors:  Do Yeon Kim
+#           Irvin Steve Cardenas (@kpatch)
+
 # this is 'test' version (ver1)
 ## import
 import sys
 sys.path.append("/Users/dkim/Documents/Programming/Python/pyserial-3.4")
 import serial
 import binascii
+import csv
+# import timeit
+import time
+import pandas as pd
 # import time
 # import openbci_realtime_ver1
 
@@ -14,6 +21,8 @@ class val():
   pass
 class temp():
   pass
+# class data1():
+#   pass
 ser = serial.Serial('/dev/tty.usbserial-DM00PZYR') # open serial port tty.usbserial-DM00PZYR
 num_sample = ""
 num_ch = 9 # 6 ch + 2 reference ch
@@ -38,10 +47,16 @@ print(byte1.start1)
 
 
 ## real time
+temp_trial = 1
+end_trial = 4
 with serial.Serial(ser.name, 115200, timeout = 1, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE) as ser1: 
   # button: ON
   ser1.write('b')
-  while(1): 
+  temp_block = 1
+  startTime = time.time()
+
+  df = pd.DataFrame(columns=['CH1','CH2','CH3','CH4','CH5','CH6','CH7', 'CH8'])
+  while(1):
     temp.s1 = ser1.read(1) # read up to ten bytes (timeout)
     if binascii.hexlify(temp.s1) == "a0":
       raw_data = ser1.read(31)
@@ -51,19 +66,37 @@ with serial.Serial(ser.name, 115200, timeout = 1, parity = serial.PARITY_NONE, s
       for temp_i in range(1, num_accel): 
         raw_accel[temp_i - 1] = raw_data[25 + 2 * (temp_i - 1):27 + 2 * (temp_i - 1)] # accel x, y, and z
       
-    # save raw_data
-    
+      ch1 = str(int(binascii.hexlify(raw_EMG[1]), 16) * val.scaleFac)
+      ch2 = str(int(binascii.hexlify(raw_EMG[2]), 16) * val.scaleFac)
+      ch3 = str(int(binascii.hexlify(raw_EMG[3]), 16) * val.scaleFac)
+      ch4 = str(int(binascii.hexlify(raw_EMG[4]), 16) * val.scaleFac)
+      ch5 = str(int(binascii.hexlify(raw_EMG[5]), 16) * val.scaleFac)
+      ch6 = str(int(binascii.hexlify(raw_EMG[6]), 16) * val.scaleFac)
+      ch7 = str(int(binascii.hexlify(raw_EMG[7]), 16) * val.scaleFac)
+      ch8 = str(int(binascii.hexlify(raw_EMG[8]), 16) * val.scaleFac)
 
-      # for temp_i in range(1, num_ch - 1): 
-      #   if temp_i == num_ch - 1: 
-      #     print(str(int(binascii.hexlify(raw_EMG[1]), 16) * val.scaleFac))
-      #   else: 
-      #     print(str(int(binascii.hexlify(raw_EMG[1]), 16) * val.scaleFac) + "\t")
-      print(str(int(binascii.hexlify(raw_EMG[1]), 16) * val.scaleFac) + "\t"
-        + str(int(binascii.hexlify(raw_EMG[2]), 16) * val.scaleFac) + "\t"
-        + str(int(binascii.hexlify(raw_EMG[3]), 16) * val.scaleFac) + "\t"
-        + str(int(binascii.hexlify(raw_EMG[4]), 16) * val.scaleFac) + "\t"
-        + str(int(binascii.hexlify(raw_EMG[5]), 16) * val.scaleFac) + "\t"
-        + str(int(binascii.hexlify(raw_EMG[6]), 16) * val.scaleFac) + "\t"
-        + str(int(binascii.hexlify(raw_EMG[7]), 16) * val.scaleFac) + "\t"
-        + str(int(binascii.hexlify(raw_EMG[8]), 16) * val.scaleFac))
+      # save raw_data
+      #  print(ch1 + "\t" + ch2 + "\t" + ch3 + "\t" + ch4 + "\t" + ch5 + "\t" + ch6 + "\t" + ch7 + "\t" + ch8)
+
+      # df2 = pd.DataFrame([ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8])
+      channels =[{'CH1': ch1, 'CH2': ch2, 'CH3': ch3, 'CH4': ch4, 'CH5': ch5, 'CH6': ch6, 'CH7': ch7, 'CH8': ch8}]
+      df2 = pd.DataFrame(channels)
+      df = df.append(df2)
+      endTime = time.time()
+      # print("DIFF: %f", (endTime - startTime))
+      if( (endTime - startTime) >= 5):
+        # df = pd.DataFrame(columns=['Ch1','Ch2','Ch3','Ch4','Ch5','Ch6','Ch7', 'Ch8'])
+        startTime = endTime
+        print(df)
+        df = df.iloc[0:0]
+        # df = pd.DataFrame(columns=df.columns)
+      
+    
+      
+
+    temp_block += 1
+    if temp_trial == end_trial: 
+      # collect data
+      print('{0:2f} {1:3f} {2:4f}'.format(str(int(binascii.hexlify(raw_EMG[1]), 16) * val.scaleFac)))
+      end_trial = end_trial + 4
+      ser1.write('s')
